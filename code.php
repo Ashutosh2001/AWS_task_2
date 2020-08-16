e8a055eeb813f11ec2c35c96ebaa08564aba694f
Ashutosh Saini <55857615+Ashutosh2001@users.noreply.github.com>
2020-08-16 17:55:28 +0530

    Create code.php

---
+++ code.php
@@ -0,0 +1,6 @@
+<?php
+print ("<h2>PHP is Fun!</h2>)";
+print ("Hello world!<br>");
+print ("I'm about to learn PHP!");
+eco "<'https://github.com/Ashutosh2001/AWS_Cloud/blob/master/93863.jpg' width='300' height='140' />")
+?>
