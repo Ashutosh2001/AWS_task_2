9c8f9085d283ec2e5df35699c9a84bf6970c5ddf
Ashutosh Saini <55857615+Ashutosh2001@users.noreply.github.com>
2020-08-16 18:02:14 +0530

    Update code.php

--- code.php
+++ code.php
@@ -2,5 +2,5 @@
 print ("<h2>PHP is Fun!</h2>)";
 print ("Hello world!<br>");
 print ("I'm about to learn PHP!");
-eco "<'https://github.com/Ashutosh2001/AWS_Cloud/blob/master/93863.jpg' width='300' height='140' />")
+eco "<'creepy-dark-fog-1516202.jpg' width='300' height='140' />")
 ?>
